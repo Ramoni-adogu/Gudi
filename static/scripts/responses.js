0ec797b79957f719c37c8e89f257029c13a7de8d
Ramoni-adogu <110678757+Ramoni-adogu@users.noreply.github.com>
2023-01-10 22:38:28 +0100

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,133 @@
+function getBotResponse(input) {
+
+    // Responses for About
+    if (input == "About") {
+        return "Gudi Goodies is an online shopping store where you can purchase your favourite brands of clothings and accessories, no matter what it is," +
+            "at the most affordable and friendly prices. Your satisfaction is our utmost priority.";
+
+    } else if (input == "ABOUT") {
+        return "Gudi Goodies is an online shopping store where you can purchase your favourite brands of clothings and accessories, no matter what it is," +
+            "at the most affordable and friendly prices. Your satisfaction is our utmost priority.";
+
+    } else if (input == "about") {
+        return "Gudi Goodies is an online shopping store where you can purchase your favourite brands of clothings and accessories, no matter what it is," +
+            "at the most affordable and friendly prices. Your satisfaction is our utmost priority.";
+
+    } else if (input == "1") {
+        return "Gudi Goodies is an online shopping store where you can purchase your favourite brands of clothings and accessories, no matter what it is," +
+            "at the most affordable and friendly prices. Your satisfaction is our utmost priority.";
+    }
+
+    // Responses for Services
+    if (input == "Services") {
+        return "Our services are top notch. We sell unisex clothings, shoes, bags and other accessories like scarfs, jewelry, etc." +
+            "<br>" + "Feel free to check our Shop Page for more!";
+
+    } else if (input == "services") {
+        return "Our services are top notch. We sell unisex clothings, shoes, bags and other accessories like scarfs, jewelry, etc." +
+            "<br>" + "Feel free to check our Shop Page for more!";
+
+    } else if (input == "SERVICES") {
+        return "Our services are top notch. We sell unisex clothings, shoes, bags and other accessories like scarfs, jewelry, etc." +
+            "<br>" + "Feel free to check our Shop Page for more!";
+
+    } else if (input == "2") {
+        return "Our services are top notch. We sell unisex clothings, shoes, bags and other accessories like scarfs, jewelry, etc." +
+            "<br>" + "Feel free to check our Shop Page for more!";
+    }
+
+    //Responses for Delivery
+    if (input == "Delivery") {
+        return "1. Where can we deliver to?" + "<br>" + "2. Is there any charge for delivery?" + "<br>" + "3. How long does it take to get product delivered?";
+
+    } else if (input == "delivery") {
+        return "1. Where can we deliver to?" + "<br>" + "2. Is there any charge for delivery?" + "<br>" + "3. How long does it take to get product delivered?";
+
+    } else if (input == "DELIVERY") {
+        return "1. Where can we deliver to?" + "<br>" + "2. Is there any charge for delivery?" + "<br>" + "3. How long does it take to get product delivered?";
+
+    } else if (input == "3") {
+        return "Please choose a letter to continue." + "<br>" + "a) Where can we deliver to?" + "<br>" + "b) Is there any charge for delivery?" + "<br>" + "c) How long does it take to get product delivered?";
+
+    } else if (input == "a") {
+        return "We offer delivery throughout Nigeria.";
+
+    } else if (input == "b") {
+        return "Yes, delivery is not free, except you have a free-delivery coupon. Delivery fee however varies by location. In Nigeria, delivery fee ranges between ₦500 - ₦2,000."
+
+    } else if (input == "c") {
+        return "Product delivery time depends on the location, accessibility and type of goods to be delivered. These factors may affect delivery time, but it shouldn't take more than a week to get your goods" +
+            "delivered to you. You can track my order to see the progress of your delivery."
+    }
+
+    // Responses for My Account 
+    if (input == "My Account") {
+        return "You will need to create an account before you can complete your order. When logged in, navigate to user page to view your profile." +
+            "<br>" + "<br>" + "If you have an issue with your user profile, logging in our signing up, please send us an email at" + "<strong>" + " " +
+            "gudigoodies@gmail.com.";
+
+    } else if (input == "My account") {
+        return "You will need to create an account before you can complete your order. When logged in, navigate to user page to view your profile." +
+            "<br>" + "<br>" + "If you have an issue with your user profile, logging in our signing up, please send us an email at" + "<strong>" + " " +
+            "gudigoodies@gmail.com.";
+
+    } else if (input == "my Account") {
+        return "You will need to create an account before you can complete your order. When logged in, navigate to user page to view your profile." +
+            "<br>" + "<br>" + "If you have an issue with your user profile, logging in our signing up, please send us an email at" + "<strong>" + " " +
+            "gudigoodies@gmail.com.";
+
+    } else if (input == "MY ACCOUNT") {
+        return "You will need to create an account before you can complete your order. When logged in, navigate to user page to view your profile." +
+            "<br>" + "<br>" + "If you have an issue with your user profile, logging in our signing up, please send us an email at" + "<strong>" + " " +
+            "gudigoodies@gmail.com.";
+
+    } else if (input == "my account") {
+        return "You will need to create an account before you can complete your order. When logged in, navigate to user page to view your profile." +
+            "<br>" + "<br>" + "If you have an issue with your user profile, logging in our signing up, please send us an email at" + "<strong>" + " " +
+            "gudigoodies@gmail.com.";
+
+    } else if (input == "4") {
+        return "You will need to create an account before you can complete your order. When logged in, navigate to user page to view your profile." +
+            "<br>" + "<br>" + "If you have an issue with your user profile, logging in our signing up, please send us an email at" + "<strong>" + " " +
+            "gudigoodies@gmail.com.";
+    }
+
+    // Report Issue
+    if (input == "Report issue") {
+        return "We are sorry if you are experiencing any issue on our site." + "<br>" + "Please send us a message on our email (gudigoodies@gmail.com)" +
+            "<br>" + "Call us (+234 (0) 906 073 9556)" + "<br>" + "Or Leave a WhatsApp Message." + "<br>" + "<br>" +
+            "We'd be more than glad to assist you!"
+    } else if (input == "Report Issue") {
+        return "We are sorry if you are experiencing any issue on our site." + "<br>" + "Please send us a message on our email (gudigoodies@gmail.com)" +
+            "<br>" + "Call us (+234 (0) 906 073 9556)" + "<br>" + "Or Leave a WhatsApp Message." + "<br>" + "<br>" +
+            "We'd be more than glad to assist you!"
+    } else if (input == "report issue") {
+        return "We are sorry if you are experiencing any issue on our site." + "<br>" + "Please send us a message on our email (gudigoodies@gmail.com)" +
+            "<br>" + "Call us (+234 (0) 906 073 9556)" + "<br>" + "Or Leave a WhatsApp Message." + "<br>" + "<br>" +
+            "We'd be more than glad to assist you!"
+    } else if (input == "report Issue") {
+        return "We are sorry if you are experiencing any issue on our site." + "<br>" + "Please send us a message on our email (gudigoodies@gmail.com)" +
+            "<br>" + "Call us (+234 (0) 906 073 9556)" + "<br>" + "Or Leave a WhatsApp Message." + "<br>" + "<br>" +
+            "We'd be more than glad to assist you!"
+    } else if (input == "REPORT ISSUE") {
+        return "We are sorry if you are experiencing any issue on our site." + "<br>" + "Please send us a message on our email (gudigoodies@gmail.com)" +
+            "<br>" + "Call us (+234 (0) 906 073 9556)" + "<br>" + "Or Leave a WhatsApp Message." + "<br>" + "<br>" +
+            "We'd be more than glad to assist you!"
+    } else if (input == "5") {
+        return "We are sorry if you are experiencing any issue on our site." + "<br>" + "Please send us a message on our email (gudigoodies@gmail.com)" +
+            "<br>" + "Call us (+234 (0) 906 073 9556)" + "<br>" + "Or Leave a WhatsApp Message." + "<br>" + "<br>" +
+            "We'd be more than glad to assist you!"
+    }
+
+    // FAQ
+    if (input == "FAQ") {
+        return "Frequently Asked Questions" +
+            "<br>" + "<br>" + ""
+    }
+
+    // Invalid Message Reply
+    else {
+        return "1. About" + "<br>" + "2. Services" + "<br>" + "3. Delivery" + "<br>" + "4. My Account" + "<br>" +
+            "5. Report issue" + "<br>" + "6. FAQ";
+    }
+}
